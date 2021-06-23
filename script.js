c64c0f12fe3a0a12dbee359be7f71afab9041ffb
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-06-23 19:37:18 -0400

    Added passwordOptions object. Added buildPassword function

--- script.js
+++ script.js
@@ -1,48 +1,63 @@
-const specialCharacters = [ "@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".", ];
-// Array of numeric characters to be included in password
-const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-// Array of lowercase characters to be included in password
-const lowerCasedCharacters = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ];
-
-// Array of uppercase characters to be included in password
-const upperCasedCharacters = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ];
-
-
+// Assignment Code
+const generateBtn = document.querySelector("#generate");
 
 generatePassword = () => {
   const options = getPasswordOptions();
-  console.log(options)
-}
+  console.log(options);
+  let password = buildPassword(options) 
+};
 
 getPasswordOptions = () => {
-  let passwordLength = parseInt(prompt("Enter length of password"));
-  if(passwordLength < 8 || passwordLength > 128) {
+  const passwordLength = parseInt(prompt("Enter length of password"));
+  if (passwordLength < 8 || passwordLength > 128) {
     alert("Your password must be between 8 and 128 characters");
     return null;
   }
-  let containsSpecialCharacters = confirm("Click OK to confirm special characters in your password");
-  let containsNumericCharacters = confirm("Click OK to confirm including numeric characters.");
-  let containsLowerCasedCharacters = confirm("Click OK to confirm including lowercase characters.");
-  let cointainsUpperCasedCharacters = confirm("Click OK to confirm including uppercase characters.");
+  const containsSpecialCharacters = confirm( "Click OK to confirm special characters in your password" );
+  const containsNumericCharacters = confirm("Click OK to confirm including numeric characters.");
+  const containsLowerCasedCharacters = confirm("Click OK to confirm including lowercase characters.");
+  const containsUpperCasedCharacters = confirm( "Click OK to confirm including uppercase characters." );
 
-  if(containsSpecialCharacters === false && containsNumericCharacters === false && containsLowerCasedCharacters === false && cointainsUpperCasedCharacters === false) {
-    alert("You must select at least one character set")
-    return null;
+  if (
+    containsSpecialCharacters === false &&
+    containsNumericCharacters === false &&
+    containsLowerCasedCharacters === false &&
+    cointainsUpperCasedCharacters === false
+  ) {
+    alert("You must select at least one character set");
+    return;
   }
-  
+  const passwordOptions = {
+    length: passwordLength,
+    special: containsSpecialCharacters,
+    numeric: containsNumericCharacters,
+    lower: containsLowerCasedCharacters,
+    upper: containsUpperCasedCharacters
+  }
+
+  return passwordOptions;
 };
 
-// Assignment Code
-let generateBtn = document.querySelector("#generate");
+
+buildPassword = ({length, special, numeric, lower, upper}) => {
+  let possibleCharSet = [];
+  console.log(length, special, numeric, lower, upper)
+  
+}
+
+random = (options) => {
+  const index = Math.floor(Math.random() * options.length);
+  const randomChar = options[index];
+  return randomChar;
+}
 
 // Write password to the #password input
 writePassword = () => {
-  let password = generatePassword();
+  const password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
